de479b1aaea968bf8314129ea76f829ae667ffe9
Seb Kay <seb@sebkay.com>
2022-04-26 17:59:21 +0100

    Move form elements to Blade components

---
+++ resources/views/components/forms/fields/image.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <label for="file-upload" class="block text-lg font-medium text-gray-700">{{ $label }}</label>
+    <div class="mt-3">
+        <input id="file-upload" class="border-solid border-2 border-gray-300 rounded-md p-1.5" name="file-upload"
+            type="file">
+    </div>
+</div>
